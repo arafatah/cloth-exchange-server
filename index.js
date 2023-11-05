ae8e887c6c47e81cdc97a0b7a3880ef51c04d76c
arafatah <arafathowlader456@gmail.com>
2023-11-05 23:50:38 +0600

    delete button implement

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.json(service);
     });
 
+    app.delete("/delete/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await serviceCollection.deleteOne(query);
+        res.json(result);
+        });
+
     app.post("/addOrder", async (req, res) => {
       const order = req.body;
       const result = await cartCollection.insertOne(order);
